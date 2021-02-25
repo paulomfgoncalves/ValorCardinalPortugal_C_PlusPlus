9fc0da2cf9a3b52a8fa05d5a2e55e03ac1a90ac1
Paulo Manuel Francisco Goncalves <paulomfgoncalves@hotmail.com>
2021-02-25 14:53:05 +0000

    main.cpp format source

--- ValorCardinalPortugal/Main.cpp
+++ ValorCardinalPortugal/Main.cpp
@@ -5,57 +5,54 @@
 
 #define TEST
 
-/* MAIN */
 int main(int argc, char* argv[])
 {
-#ifdef TEST
-    tests();
-    return 0;
-#endif
-       
+	#ifdef TEST
+		tests();
+		return 0;
+	#endif
 
+	bool vazioSeZeroParteinteira = false;
+	bool vazioSeZeroParteDecimal = false;
 
-    bool vazioSeZeroParteinteira = false;
-    bool vazioSeZeroParteDecimal = false;
+	if (argc >= 3)
+	{
+		string temp = strupper(argv[2]);
+		vazioSeZeroParteinteira = (temp.compare("TRUE") == 0);
+	}
 
-    if (argc >= 3)
-    {
-        string temp = strupper(argv[2]);
-        vazioSeZeroParteinteira = (temp.compare("TRUE") == 0);
-    }
+	if (argc >= 4)
+	{
+		string temp = strupper(argv[3]);
+		vazioSeZeroParteDecimal = (temp.compare("TRUE") == 0);
+	}
 
-    if (argc >= 4)
-    {
-        string temp = strupper(argv[3]);
-        vazioSeZeroParteDecimal = (temp.compare("TRUE") == 0);
-    }
+	if (argc >= 2)
+	{
+		string result = Converte(argv[1], vazioSeZeroParteinteira, vazioSeZeroParteDecimal);
+		cout << "[" + result + "]" << endl;
+		return 0;
+	}
 
-    if (argc >= 2)
-    {
-        string result = Converte(argv[1], vazioSeZeroParteinteira, vazioSeZeroParteDecimal);
-        cout << "[" + result + "]" << endl;
-        return 0;
-    }
+	//printf("\n");
+	//printf("ValorCardinalPortugal (Version: %s)\n", VERSION);
+	//printf("======================================\n");
+	//printf("\n");
 
-    //printf("\n");
-    //printf("ValorCardinalPortugal (Version: %s)\n", VERSION);
-    //printf("======================================\n");
-    //printf("\n");
+	//char str[255];
+	//printf("Introduza valor '#0.00': ");
+	//scanf("%s", str);
 
-    //char str[255];
-    //printf("Introduza valor '#0.00': ");
-    //scanf("%s", str);
+	//printf("\nProcessa :[%s]\n", str);
 
-    //printf("\nProcessa :[%s]\n", str);
+	//char* valor = str;
+	////char *valor = &str;
 
-    //char* valor = str;
-    ////char *valor = &str;
+	//char* result = converte(valor, vazioSeZeroParteinteira, vazioSeZeroParteDecimal);
+	//printf("\nResultado:[%s]\n", result);
 
-    //char* result = converte(valor, vazioSeZeroParteinteira, vazioSeZeroParteDecimal);
-    //printf("\nResultado:[%s]\n", result);
+	//printf("\nEnter\n");
+	//getchar();
 
-    //printf("\nEnter\n");
-    //getchar();
-
-    //return 0;
+	//return 0;
 }
